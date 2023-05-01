1d9d0f6838f72b9f4770333a66571a1d6e697736
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-05-01 23:32:48 +0300

    added(PM2 config)

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pean.api',
+      script: './dist/main.js',
+      args: '',
+      kill_timeout: 4000,
+      wait_ready: true,
+      autorestart: true,
+      watch: ['src'],
+      ignore_watch: ['node_modules', 'dist'],
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      'pre-start': 'npm i && npm run build',
+    },
+  ],
+};
